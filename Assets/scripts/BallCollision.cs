778e918123c8f058244a78cf452cdaffcfc51fa0
mathis van der steen <mathisvdersteen@hotmail.com>
2025-10-01 22:43:13 +0200

    added ball

---
+++ Assets/scripts/BallCollision.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallCollision : MonoBehaviour
+{
+    private bounce bounce;
+
+    void Start()
+    {
+        bounce = GetComponent<bounce>();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Spikes"))
+        {
+            Debug.Log("Spikes");
+        }
+        else if (other.gameObject.CompareTag("Platform"))
+        {
+            Debug.Log("Platform");
+            bounce.Bounce();
+        }
+    }
+}
---
+++ Assets/scripts/bounce.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bounce : MonoBehaviour
+{
+    public float bounceForce = 15f;
+    private Rigidbody2D rb;
+
+    //functions 
+    public void Bounce()
+    {
+        Debug.Log("Bounce");
+        rb.velocity = new Vector2(rb.velocity.x, bounceForce);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
